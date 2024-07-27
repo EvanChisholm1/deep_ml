d521baf7001b60d943221627b05b8d5dce28e8a3
Evan Chisholm <elchisholm248@gmail.com>
2024-07-26 21:41:31 -0400

    completed linear regression with gradient descent

---
+++ linear_regression_grad/main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def linear_regression_gradient_descent(X: np.ndarray, y: np.ndarray, alpha: float, iterations: int) -> np.ndarray:
+    m, n = X.shape
+    theta = np.zeros((n, 1))
+
+    for i in range(iterations):
+        preds = X @ theta
+        err = preds - y.reshape(-1, 1)
+        theta -= alpha * (X.T @ err / m)
+    
+    return theta.flatten().round(4)
+
+print(linear_regression_gradient_descent(X = np.array([[1, 1], [1, 2], [1, 3]]), y = np.array([1, 2, 3]), alpha = 0.01, iterations = 1000))
